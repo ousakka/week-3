8debe8da263a9ca4a1cb91a085399a078214d052
ousakka <oumaima.sakka.os@gmail.com>
2025-03-21 21:51:46 +0100

    add homework

---
+++ homework/script.js
@@ -0,0 +1,17 @@
+function Restaurant(nameOfTheRestaurant, amount, food) {
+  alert(
+    " You are ordering " +
+      amount +
+      " of " +
+      food +
+      "from " +
+      nameOfTheRestaurant
+  );
+  console.log(
+    " You are ordering " + amount + " of " +food+ "  from " + nameOfTheRestaurant
+  );
+}
+
+Restaurant("italien food", 1, "Pizza margherita ");
+Restaurant("Doner King", 1, "Doner ");
+Restaurant("Soltan", 1, "Shawarma ");
